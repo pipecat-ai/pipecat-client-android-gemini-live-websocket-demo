8609635b4e0190341fc25cf102f80f33522770d5
marcus-daily <111281783+marcus-daily@users.noreply.github.com>
2025-01-09 15:16:51 +0000

    Gemini Multimodal websocket example app

---
+++ README.md
@@ -0,0 +1,6 @@
+# Android Gemini Multimodal Live Websocket Demo
+
+Demo app for using the Pipecat Android client to connect to Gemini
+Multimodal Live over a Websocket connection.
+
+<img alt="screenshot" src="files/screenshot.png" width="600px" />
---
+++ settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        mavenLocal()
+    }
+}
+
+rootProject.name = "Gemini Multimodal Live Websocket Demo"
+include(":gemini-multimodal-websocket-demo")
